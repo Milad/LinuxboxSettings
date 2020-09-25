0ff3b1be95ebe739e0dd3c00198a046a180922cf
Milad Kawas <Milad@users.noreply.github.com>
2020-09-25 12:24:27 +0200

    Better bash file

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-while [ "$1" != "" ]; do
-    case $1 in
-    up)
-        swanctl -i -c zmarta
-        ;;
-    status)
-        swanctl -l
-        ;;
-    down)
-        swanctl -t -c zmarta
-        ;;
-    esac
-    shift
-done
-
+case "$1" in
+up)
+    swanctl -i -c zmarta
+    ;;
+status)
+    swanctl -l
+    ;;
+down)
+    swanctl -t -c zmarta
+    ;;
+*)
+    echo Please supply either: up down status
+    exit 1
+    ;;
+esac
